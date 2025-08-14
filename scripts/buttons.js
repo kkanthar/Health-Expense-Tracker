aff92d0b5819f2d27e039436bee45e095fca2b7b
kkanthar <keerthanankantharuban@gmail.com>
2025-08-14 16:29:44 -0400

    -Finished add expense page, tweaked type button and content, need to optimize css
    -Added header to my expense page to state what type of expense and corresponding color

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -5,6 +5,11 @@ const dropdownContent = document.getElementById('dropdownContent')
 
 dropdownButton.addEventListener('click', () => {
     dropdownContent.classList.toggle('show');
+    if (dropdownContent.classList.contains('show')){
+        dropdownButton.style.borderRadius = "3px 3px 0px 0px"
+    } else {
+        dropdownButton.style.borderRadius = "3px 3px 3px 3px"
+    }
 });
 
 function changeButtonText(text){
@@ -12,6 +17,27 @@ function changeButtonText(text){
     dropdownButton.innerText = text;
     dropdownButton.value = text;
     dropdownContent.classList.remove('show');
+
+    dropdownButton.style.borderRadius = "3px 3px 3px 3px"
+    
+
+
+
+    if (text === "Prescription"){
+        
+        dropdownButton.style.backgroundColor = "rgb(221, 37, 0)";
+        
+    } else if (text === "Counter Medicine"){
+        dropdownButton.style.backgroundColor = "rgb(255, 193, 7)";
+        
+
+    } else if (text === "Check Up"){
+        dropdownButton.style.backgroundColor = "rgb(76, 175, 80)";
+    } else if (text === "Insurance"){
+        dropdownButton.style.backgroundColor = "rgb(33, 150, 243)";
+    } else if (text === "Other"){
+        dropdownButton.style.backgroundColor = "rgb(158, 158, 158)";
+    }
 
 
 }
